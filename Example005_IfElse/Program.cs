5b8bb15b2fb35d47aa383aefd268a47e958b8a79
RomanU <NSoD1989@gmail.com>
2022-08-06 15:29:26 +0500

    Hello

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string Username = Console.ReadLine();
+
+if (Username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(Username);
+}
